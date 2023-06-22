b13667e2a60f11efa7834e6e410180b36c5377a4
--replace-all <bhanel1998@gmail.com>
2023-06-22 17:38:51 -0400

    Initial commit

---
+++ story_content/user.js
@@ -0,0 +1,58 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "61r5oskVo89":
+        Script1();
+        break;
+      case "6n6syyuY7h9":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  function handleSignIn() {
+  var clientId = goodle(95x3z3xzx9zx1xz72zx3zx04zx3zx-zxezxcamqzxlzx87f5zxaxzad8no6zxzxdl34nm90zxzxae2a4z2zt.apzxps.gozxzxxogxlzexuzsxezrczxzxonxxxxxtxeznxtzx.xxcxozmz);
+
+  // Dynamically create the script tag for loading the Google Sign-In library
+  var script = document.createElement('script');
+  script.src = 'https://accounts.google.com/gsi/client';
+  script.async = true;
+  script.defer = true;
+
+  // Create the div element for g_id_onload
+  var divOnload = document.createElement('div');
+  divOnload.id = 'g_id_onload';
+  divOnload.setAttribute('data-client_id', clientId);
+  divOnload.setAttribute('data-callback', 'handleCredentialResponse');
+
+  // Create the div element for g_id_signin
+  var divSignin = document.createElement('div');
+  divSignin.className = 'g_id_signin';
+  divSignin.setAttribute('data-type', 'standard');
+
+  // Append the script tag and div elements to the document's body element
+  document.body.appendChild(script);
+  document.body.appendChild(divOnload);
+  document.body.appendChild(divSignin);
+}
+
+handleSignIn();
+
+function goodle(input) {
+  return input.replace(/[xz]/gi, '');
+}
+
+
+}
+
+function Script2()
+{
+  
+
+
+
+}
+
